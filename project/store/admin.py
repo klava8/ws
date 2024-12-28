bd8307cd78a9379b070f90114287d7617308c7ac
ma1andes <fxckybtw@gmail.com>
2024-12-28 14:38:22 +0300

    start store models

--- project/store/admin.py
+++ project/store/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Product, Category
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'category') 
+    list_display_links = ('name', 'price')
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category')
--- project/store/models.py
+++ project/store/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
 # Create your models here.
+
+
+CATEGORY = [
+    ('guppy', 'Гуппи'),
+    ('carps', 'Карпы'),
+    ('perches', 'Окуни'),
+    ('pike', 'Щуки'),
+]
+
+
+class Product(models.Model):
+    name = models.CharField(
+        verbose_name='Наименование товара',
+        max_length=255,
+    )
+    price = models.FloatField(
+        verbose_name='Цена товара'
+    )
+    category = models.ForeignKey(
+        to='Category',
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True
+    )
+    image = models.ImageField(
+        upload_to='#'
+        )
+    
+    def __str__(self):
+        return self.name
+    
+    def full(self):
+        return self.name + ' ' + self.price + ' ' + self.category + 'br' + self.image
+
+class Category(models.Model):
+    category = models.CharField(
+        max_length=125,
+        choices=CATEGORY,
+        verbose_name='Категория'
+    )
+
+    def __str__(self):
+        return self.category
